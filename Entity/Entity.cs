289c55422485d5e2565d6266e119737dc0379f7e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-10 19:34:48 +1000

    Beginnings of the static class.

--- Entity/Entity.cs
+++ Entity/Entity.cs
@@ -1,10 +1,71 @@
 ﻿using System;
-namespace ECS
+using System.Collections.Generic;
+using ECS.Components;
+
+namespace ECS.Entities
 {
-	public class Entity
+	public static class Entity
 	{
-		public Entity()
+		static int maxEntityId = 0;
+		static Dictionary<int, Component[]> _entities = new Dictionary<int, Component[]> { };
+
+		static int numberOfComponents = Enum.GetValues(typeof(ComponentType)).Length;
+		static Type[] _types = new Type[] {typeof(FooComponent), typeof(BarComponent) };
+
+		public static bool HasComponent(int entityId, ComponentType componentType)
 		{
+			if (!_entities.ContainsKey(entityId))
+			{
+				Console.WriteLine(string.Format("Unknown Entity ID: {0}", entityId));
+				return false;
+			}
+
+			int index = (int)componentType;
+			return _entities[entityId][index] != null;
+		}
+
+		public static int CreateEntity()
+		{
+			int entityID = maxEntityId;
+			maxEntityId++;
+			_entities[entityID] = new Component[numberOfComponents];
+			return entityID;
+		}
+
+		public static void DestroyEntity(int entityId)
+		{
+			if (_entities.ContainsKey(entityId))
+			{
+				var componentArray = _entities[entityId];
+
+				for (int i = 0; i < numberOfComponents; i++)
+				{
+					if (componentArray[i] != null)
+						RemoveComponent(entityId, (ComponentType)i);
+				}
+
+				_entities.Remove(entityId);
+			}
+		}
+
+		public static void AddComponent(int entityId, Component component)
+		{
+			if (_entities.ContainsKey(entityId))
+			{
+				if (_entities[entityId][(int)component.componentType] != null)
+					RemoveComponent(entityId, component.componentType);
+				_entities[entityId][(int)component.componentType] = component;
+			}
+			else
+				Console.WriteLine(string.Format("Tried to add component to unknown Entity ID: {0}", entityId));
+		}
+
+		public static void RemoveComponent(int entityId, ComponentType componentType)
+		{
+			if (_entities.ContainsKey(entityId))
+				_entities[entityId][(int)componentType] = null;
+			else
+				Console.WriteLine(string.Format("Unknown Entity ID: {0}", entityId));
 		}
 	}
 }
