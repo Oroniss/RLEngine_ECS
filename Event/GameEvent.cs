2d020783cd3358b465db5085d886cbaa0bf3ed7b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-11 09:56:26 +1000

    Started fleshing out some basic details on events.

--- Event/GameEvent.cs
+++ Event/GameEvent.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
-namespace ECS.Events
+namespace ECS.GameEvents
 {
-	public class Event
+	public class GameEvent
 	{
-		public Event()
+		static GameEvent[] _recentEvents;
+		static int _eventIndex;
+		static List<GameEvent> _eventsToProcess;
+
+		public GameEvent()
 		{
+		}
+
+
+
+		public static void AddEvent(GameEvent gameEvent)
+		{
+			_recentEvents[_eventIndex] = gameEvent;
+			_eventIndex = (_eventIndex + 1) % _recentEvents.Length;
+
+			_eventsToProcess.Add(gameEvent);
+
+			if (_eventsToProcess.Count == 1)
+			{
+				while (_eventsToProcess.Count > 0)
+				{
+					// TODO: Check for listeners here.
+					_eventsToProcess.RemoveAt(0);
+				}
+			}
+
 		}
 	}
 }
