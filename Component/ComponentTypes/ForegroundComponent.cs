df24d950304fae5aa1c3c7662303568a183c9bca
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-23 23:14:13 +1000

    Created component for foreground drawing information.

--- Component/ComponentTypes/ForegroundComponent.cs
+++ Component/ComponentTypes/ForegroundComponent.cs
@@ -1,10 +1,43 @@
 ﻿using System;
-namespace ECS
+
+namespace ECS.Components
 {
-	public class ForegroundComponent
+	[Serializable]
+	public class ForegroundComponent:Component
 	{
-		public ForegroundComponent()
+		char _symbol;
+		string _fgColor;
+		ForegroundDisplayLayer _displayLayer;
+
+		public ForegroundComponent(int entityId, char symbol, string fgColor, ForegroundDisplayLayer layer)
+			:base(ComponentType.Foreground, entityId)
 		{
+			_symbol = symbol;
+			_fgColor = fgColor;
+			_displayLayer = layer;
 		}
+
+		public char Symbol
+		{
+			get { return _symbol; }
+		}
+
+		public string FGColor
+		{
+			get { return _fgColor; }
+		}
+
+		public ForegroundDisplayLayer DisplayLayer
+		{
+			get { return _displayLayer; }
+		}
+	}
+
+	public enum ForegroundDisplayLayer
+	{
+		Furnishing,
+		Actor,
+		Player,
+		Interface
 	}
 }
