6b36a420f99efb390f9eaddcd52bb6cbf87c053e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-24 10:27:43 +1000

    Added system related information and updated events to actual register with listeners.

--- Event/GameEvent.cs
+++ Event/GameEvent.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections.Generic;
 using ECS.GameSystems;
+using System;
 
 namespace ECS.GameEvents
 {
 	public abstract class GameEvent
 	{
-		static GameEvent[] _recentEvents;
-		static int _eventIndex;
-		static List<GameEvent> _eventsToProcess;
+		static bool setupRequired = true;
+		static SortedDictionary<EventType, List<GameSystem>> listeners;
 
-		public GameEvent()
+		static readonly int _maxEvents = 256; // TODO: Keep an eye on this - it may depend on how many events are generated.
+		static GameEvent[] _recentEvents = new GameEvent[_maxEvents];
+		static int _eventIndex;
+		static List<GameEvent> _eventsToProcess = new List<GameEvent>();
+
+		EventType _eventType;
+
+		public GameEvent(EventType eventType)
 		{
+			_eventType = eventType;
+		}
+
+		public EventType EventType
+		{
+			get { return _eventType; }
 		}
 
 		protected static void AddEvent(GameEvent gameEvent)
@@ -24,16 +37,31 @@ namespace ECS.GameEvents
 			{
 				while (_eventsToProcess.Count > 0)
 				{
-					// TODO: Check for listeners here.
+					GameEvent processing = _eventsToProcess[0];
+					foreach (GameSystem system in listeners[processing.EventType])
+						system.ProcessEvent(processing);
 					_eventsToProcess.RemoveAt(0);
 				}
 			}
-
 		}
 
 		public static void RegisterSystem(GameSystem gameSystem, EventType[] watchedEvents)
 		{
+			if (setupRequired)
+				SetupListeners();
+
+			foreach (EventType eventType in watchedEvents)
+				listeners[eventType].Add(gameSystem);
+		}
+
+		static void SetupListeners()
+		{
+			listeners = new SortedDictionary<EventType, List<GameSystem>>();
 			
+			foreach (EventType eventType in Enum.GetValues(typeof(EventType)))
+				listeners[eventType] = new List<GameSystem>();
+
+			setupRequired = false;
 		}
 	}
 }
