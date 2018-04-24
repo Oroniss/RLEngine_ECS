cea1ec7bacc53ad8b8cca4bb2991a427e63d6373
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-24 22:25:07 +1000

    Created new events for testing purposes.

--- Event/EventTypes/MovementEvent.cs
+++ Event/EventTypes/MovementEvent.cs
@@ -1,10 +1,55 @@
 ﻿using System;
-namespace ECS
+
+namespace ECS.GameEvents
 {
-	public class MovementEvent
+	[Serializable]
+	public class MovementEvent:GameEvent
 	{
-		public MovementEvent()
+		int _entityId;
+		int _oldX;
+		int _oldY;
+		int _newX;
+		int _newY;
+
+		MovementEvent(int entityId, int oldX, int oldY, int newX, int newY)
+			:base(EventType.EntityMovement)
 		{
+			_entityId = entityId;
+			_oldX = oldX;
+			_oldY = oldY;
+			_newX = newX;
+			_newY = newY;
+		}
+
+		public int EntityId
+		{
+			get { return _entityId; }
+		}
+
+		public int OldX
+		{
+			get { return _oldX; }
+		}
+
+		public int OldY
+		{
+			get { return _oldY; }
+		}
+
+		public int NewX
+		{
+			get { return _newX; }
+		}
+
+		public int NewY
+		{
+			get { return _newY; }
+		}
+
+		public static void NewMovementEvent(int entityId, int oldX, int oldY, int newX, int newY)
+		{
+			var newEvent = new MovementEvent(entityId, oldX, oldY, newX, newY);
+			AddEvent(newEvent);
 		}
 	}
 }
