bf4f4956f7a979796a9d6838c08b1533773fd6fe
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-25 23:18:57 +1000

    Added as a separate class - better than having static methods on the system base class.

--- GameSystem/SystemProvider.cs
+++ GameSystem/SystemProvider.cs
@@ -1,10 +1,18 @@
-﻿using System;
-namespace ECS
+﻿namespace ECS.GameSystems
 {
-	public class SystemProvider
+	public static class SystemProvider
 	{
-		public SystemProvider()
+		static LevelSystem levelSystem = new LevelSystem();
+		static MovementSystem movementSystem = new MovementSystem();
+
+		public static LevelSystem LevelSystem
 		{
+			get { return levelSystem; }
+		}
+
+		public static MovementSystem MovementSystem
+		{
+			get { return movementSystem; }
 		}
 	}
 }
