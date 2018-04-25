1d78dd647b8eec08eeb098896c3ae25c7393e88a
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-25 23:19:59 +1000

    Trimmed all the static methods and attributes of the GameSystem base class - mostly moved to the SystemProvider class.

--- GameSystem/GameSystem.cs
+++ GameSystem/GameSystem.cs
@@ -1,21 +1,14 @@
 ﻿using ECS.GameEvents;
-using System.Collections.Generic;
 
 namespace ECS.GameSystems
 {
 	public abstract class GameSystem
 	{
-		static List<GameSystem> systemsToUpdate = new List<GameSystem>();
-		static SortedDictionary<string, GameSystem> systems = new SortedDictionary<string, GameSystem>();
-
 		string _systemName;
 		EventType[] _watchedEvents;
 
 		public GameSystem(string systemName, EventType[] watchedEvents)
 		{
-			systemsToUpdate.Add(this);
-			systems[systemName] = this;
-
 			_systemName = systemName;
 
 			_watchedEvents = new EventType[watchedEvents.Length];
@@ -26,16 +19,6 @@ namespace ECS.GameSystems
 		}
 
 		// Any events the system should watch for.
-		public virtual void ProcessEvent(GameEvent gameEvent)
-		{
-		}
-
-		public static GameSystem GetSystem(string name)
-		{
-			if (systems.ContainsKey(name))
-				return systems[name];
-			ErrorLogger.AddDebugText("Searched for unknown GameSystem: " + name);
-			return null;
-		}
+		public virtual void ProcessEvent(GameEvent gameEvent) { }
 	}
 }
