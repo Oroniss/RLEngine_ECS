52fabdddb8057dce0aa006731dd2557c1434dbff
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-25 23:17:29 +1000

    * ECS.csproj: Minor updates
    * ECS.userprefs: Minor updates

---
+++ GameSystem/SystemProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ECS
+{
+	public class SystemProvider
+	{
+		public SystemProvider()
+		{
+		}
+	}
+}
---
+++ GameSystem/SystemTypes/MovementSystem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ECS
+{
+	public class MovementSystem
+	{
+		public MovementSystem()
+		{
+		}
+	}
+}
