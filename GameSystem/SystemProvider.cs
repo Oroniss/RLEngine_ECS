91d14ee84facb6c9dfa49a9c2bc4e5b02375240b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-27 00:30:06 +1000

    Added the entity system.

--- GameSystem/SystemProvider.cs
+++ GameSystem/SystemProvider.cs
@@ -2,8 +2,14 @@
 {
 	public static class SystemProvider
 	{
+		static EntitySystem entitySystem = new EntitySystem();
 		static LevelSystem levelSystem = new LevelSystem();
 		static MovementSystem movementSystem = new MovementSystem();
+
+		public static EntitySystem EntitySystem
+		{
+			get { return entitySystem; }
+		}
 
 		public static LevelSystem LevelSystem
 		{
