44ae381fc8a2dde7273764ea6866a2e8ab0e6aff
jeffrey <jeffrey.fisher@anu.edu.au>
2018-04-27 23:30:15 +1000

    Altered it to pass the watched events in so they are all in one place and to allow for multiple systems of the same type.

--- GameSystem/SystemProvider.cs
+++ GameSystem/SystemProvider.cs
@@ -1,10 +1,20 @@
-﻿namespace ECS.GameSystems
+﻿using ECS.GameEvents;
+
+namespace ECS.GameSystems
 {
 	public static class SystemProvider
 	{
-		static EntitySystem entitySystem = new EntitySystem();
-		static LevelSystem levelSystem = new LevelSystem();
-		static MovementSystem movementSystem = new MovementSystem();
+		static readonly EventType[] levelSystemEvents =
+		{EventType.CreateEntity, EventType.DestroyEntity, EventType.MoveEntity};
+
+		static EventType[] movementSystemEvents = 
+		{ EventType.CreateEntity, EventType.DestroyEntity, EventType.MoveEntity };
+
+		static EventType[] entitySystemEvents = { };
+
+		static EntitySystem entitySystem = new EntitySystem(entitySystemEvents);
+		static LevelSystem levelSystem = new LevelSystem(levelSystemEvents);
+		static MovementSystem movementSystem = new MovementSystem(movementSystemEvents);
 
 		public static EntitySystem EntitySystem
 		{
