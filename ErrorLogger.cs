efc9ecab573952e42173d1f12a88ea7dce7140d9
jeffrey <jeffrey.fisher@anu.edu.au>
2018-05-13 00:42:46 +1000

    * ECS.userprefs: Minor changes.
    * ErrorLogger.cs: Added some testing functionality so that Error
      messages can be pulled off the message queue and played.

--- ErrorLogger.cs
+++ ErrorLogger.cs
@@ -1,7 +1,12 @@
-﻿namespace ECS
+﻿using System.Collections.Generic;
+
+namespace ECS
 {
 	public static class ErrorLogger
 	{
+		static bool testMode;
+		static List<string> testMessages;
+
 		public static void AddDebugText(string debugText)
 		{
 			AddDebugText(debugText, "White");
@@ -9,7 +14,29 @@
 
 		public static void AddDebugText(string debugText, string color)
 		{
-			System.Console.WriteLine(debugText + ", " + color);
+			if (testMode)
+			{
+				testMessages.Add(debugText);
+			}
+			else
+			{
+				System.Console.WriteLine(debugText + ", " + color);
+			}
+		}
+
+		public static void SetToTest()
+		{
+			testMode = true;
+			testMessages = new List<string>();
+		}
+
+		public static string GetNextMessage()
+		{
+			if (testMessages.Count == 0)
+				return "";
+			var nextMessage = testMessages[0];
+			testMessages.RemoveAt(0);
+			return nextMessage;
 		}
 	}
 }
