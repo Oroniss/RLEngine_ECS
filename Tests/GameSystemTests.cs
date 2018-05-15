631087e2315a879465d81c9e7826d0f76d7fb53f
jeffrey <jeffrey.fisher@anu.edu.au>
2018-05-15 21:30:39 +1000

    * ECS.userprefs: Minor updates.
    * GameSystemTests.cs: Not completed - needs more functionality to
      actually implement any significant testing.

--- Tests/GameSystemTests.cs
+++ Tests/GameSystemTests.cs
@@ -1,10 +1,34 @@
-﻿using System;
-namespace ECS
+﻿using NUnit.Framework;
+using ECS.GameSystems;
+using ECS.GameEvents;
+
+namespace ECS.Testing
 {
+	[TestFixture]
 	public class GameSystemTests
 	{
-		public GameSystemTests()
+		[Test]
+		public void TestEntitySystem()
 		{
+			// TODO: Do this once the engine has been fleshed out a bit further.
+		}
+
+		[Test]
+		public void TestLevelSystem()
+		{
+			// TODO: Do this once the engine has been fleshed out a bit further.
+		}
+
+		[Test]
+		public void TestMovementSystem()
+		{
+			// TODO: Do this once the engine has been fleshed out a bit further.
+		}
+
+		[Test]
+		public void TestGameSystemErrorHandling()
+		{
+			// TODO: Do this once the engine has been fleshed out a bit further.
 		}
 	}
 }
