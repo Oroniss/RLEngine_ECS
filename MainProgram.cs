f880aa4a99182e293e570347d9a32304fabf2b1b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-05-16 13:13:07 +1000

    * MainProgram.cs: Added some placeholder static variables.
    * packages.config: Added RLNet
    
    * RLEngine.csproj: Minor updates.
    
    * Palette.cs: Added the beginnings of a Palette class.
    
    * BackConsole.cs: The temporary console that stores images until it
      can be copied on to the main console.
    
    * BaseConsole.cs: A baseline template for all game consoles.
    
    * CharacterConsole.cs: Character display - not much here for the
      engine project since it will be highly customised.
    
    * MainGraphicDisplay.cs: Main graphic display - static class with all
      the other consoles hanging off it.
    
    * MapConsole.cs: Draws the current game map.
    
    * MenuConsole.cs: An overlay console for drawing menus or other large
      text displays.
    
    * TextConsole.cs: Draws the game message log.

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,11 +1,31 @@
-// Updated for ECS version 1.0.
-
 namespace RLEngine
 {
-	public class MainProgram
+	public static class MainProgram
 	{
+		static string versionNumber = "RLEngine.0.1";
+
 		public static void Main()
 		{
+		}
+
+		public static int CurrentTime
+		{
+			get { return 0; }
+		}
+
+		public static int PlayerXLoc
+		{
+			get { return 0; }
+		}
+
+		public static int PlayerYLoc
+		{
+			get { return 0; }
+		}
+
+		public static int CurrentLevelId
+		{
+			get { return 0; }
 		}
 	}
 }
---
+++ Support/Palette.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using RLNET;
+
+namespace RLEngine.Support.Palette
+{
+	public static class Palette
+	{
+		public static Dictionary<string, RLColor> colors = new Dictionary<string, RLColor>
+		{
+			{"Black",                   new RLColor(0, 0, 0)},
+			{"White",                   new RLColor(255, 255, 255)},
+			{"Silver",                  new RLColor(190, 190, 190)}
+		};
+
+		public static RLColor GetColor(string colorName)
+		{
+			if (colors.ContainsKey(colorName))
+				return colors[colorName];
+			ErrorLogger.AddDebugText("Unknown color name: " + colorName);
+			return colors["Black"];
+		}
+	}
+}
